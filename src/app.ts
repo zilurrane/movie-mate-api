bac945215df30f295d4063683a6edd06c98f2bb0
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2020-10-22 22:59:11 +0530

    Added MongoDB connection

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import { HealthCheckRoutes } from './routes/health-check.routes';
 
 class App {
 
     public app: express.Application;
     public healthCheckRoutes: HealthCheckRoutes = new HealthCheckRoutes();
+    public mongoUrl: string = <string>process.env.MONGO_CON_STRING;
 
     constructor() {
         this.app = express();
 
         this.config();
+        this.mongoSetup();
 
         this.app.use('/api/healthcheck', this.healthCheckRoutes.getAllRoutes());
     }
@@ -20,6 +23,14 @@ class App {
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
 
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then((_res: any) => {
+            console.log('--------------------------------------------------------------');
+            console.log('MongoDB connected successfully!!!');
+            console.log('--------------------------------------------------------------');
+        });
+    }
 }
 
 export default new App().app;
